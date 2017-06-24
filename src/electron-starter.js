629b80abd005c65345fe7f17d4ed12227f76c0db
Roger Perez <rogperez52@gmail.com>
2017-06-23 19:01:05 -0700

    set up init scripts

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,56 +1,55 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+const menubar = require('menubar')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
+let mb = menubar();
+let debug = true;
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+mb.on('ready', createMenuBar);
 
-  // and load the index.html of the app.
+function createMenuBar () {
   const startUrl = process.env.ELECTRON_START_URL || url.format({
     pathname: path.join(__dirname, '/../build/index.html'),
     protocol: 'file:',
     slashes: true
   });
-  win.loadURL(startUrl)
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+  mb.on('after-create-window', () => {
+    mb.window.loadURL(startUrl);
+
+    if (debug == true) { 
+      const size = mb.window.getSize();
+      createDebuggingWindow({height: size[0]+20, width: size[1], startUrl: startUrl});
+    }
+
+    mb.window.on('after-close', () => {
+      mb.window = null;
+      win = null;
+    });
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+function createDebuggingWindow ({height, width, startUrl}) {
+  win = new BrowserWindow({height: height, width: width})
+  win.webContents.openDevTools();
+  win.loadURL(startUrl);
+}
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
+mb.app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+});
