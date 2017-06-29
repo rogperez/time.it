ecf99e4a4a33b6313bd1747052c148372a8d8fd5
Roger Perez <rogperez52@gmail.com>
2017-06-28 23:38:52 -0700

    add time tracking to tasks

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,45 +1,77 @@
 import React, { Component } from 'react';
+import TaskAdder from './TaskAdder';
 
 class ItemList extends Component {
   constructor() {
     super();
     this.state = {
-      selectedTask: 'task1'
+      selectedTask: null,
+      tasks: [{name: "foo", elapsedTime: 0}],
+      logs: [],
+      startTime: null,
+      endTime: null
     };
   }
 
-  handleTaskChange = (changeEvent) => {
+  addTask = (taskName) => {
+    let newTasks = this.state.tasks;
+    newTasks.push({name: taskName, elapsedTime: 0});
     this.setState({
-      selectedTask: changeEvent.target.value
+      tasks: newTasks
     });
   }
 
+  handleTaskChange = (changeEvent) => {
+    let newState = { selectedTask: changeEvent.target.value };
+
+    if (this.state.startTime) {
+      const  elapsedTime = parseInt((new Date() - this.state.startTime)/1000, 10);
+      let totalElapsed = 0;
+
+      newState.tasks = this.state.tasks.map((task) => {
+        if (task.name === this.state.selectedTask) {
+          totalElapsed = task.elapsedTime + elapsedTime
+          return { name: task.name, elapsedTime: totalElapsed }
+        } else return task
+      });
+
+      newState.logs = this.state.logs;
+      newState.logs.push(
+        `${this.state.selectedTask}: current elapsed ${elapsedTime}, total elapsed ${totalElapsed}`
+      );
+    }
+
+    newState.startTime = new Date();
+
+    this.setState(newState);
+  }
+
   render() {
+    const tasks = this.state.tasks.map((task) =>
+      <div key={task.name}>
+        <label>
+          <input type="radio" value={task.name} 
+            onChange={this.handleTaskChange}
+            checked={this.state.selectedTask===task.name} />
+          {task.name}
+        </label>
+      </div>
+    );
+
+    const logs = this.state.logs.map((log) =>
+      <div key={log}>
+        {log}
+      </div>
+    );
+
     return (
       <div>
+        <TaskAdder addTask={this.addTask} />
         <div>
-          <label>
-            <input type="radio" value="task1" 
-              onChange={this.handleTaskChange}
-              checked={this.state.selectedTask==='task1'} />
-            Task1
-          </label>
+          {tasks}
         </div>
         <div>
-          <label>
-            <input type="radio" value="task2"
-              onChange={this.handleTaskChange}
-              checked={this.state.selectedTask==='task2'} />
-            Task2
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" value="task3"
-              onChange={this.handleTaskChange}
-              checked={this.state.selectedTask==='task3'} />
-            Task3
-          </label>
+          {logs}
         </div>
       </div>
     );
---
+++ src/components/TaskAdder.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class TaskAdder extends Component {
+  constructor() {
+    super();
+    this.state = { value: '' };
+  }
+
+  handleUpdate = (e) => {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({value: ''});
+    this.props.addTask(this.state.value);
+  }
+
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input value={this.state.value} onChange={this.handleUpdate} />
+          <input type="submit" value="Add Item" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default TaskAdder;
